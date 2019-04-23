b0670539f5adc4867a5f770fd76032df0771d900
SuperAme <beatamez@hotmail.com>
2019-04-22 20:28:31 -0500

    Se leen las series y se agregan

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const {} = require('./server')
+
+//settings
+
+app.set('port',process.env.PORT || 3000)
+
+//Middlewares
---
+++ server/server.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const SALT_I = 10
+const jwt = require('jsonwebtoken')
+const {Serie} = require('../server/models/Serie')
+const port = process.env.PORT || 3002
+
+require('dotenv').config()
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en puerto ${port}`)
+})
+
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true },(err) => {
+    if(err) return err
+    console.log("Conectado a MongoDB")
+})
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cookieParser())
+
+app.get('/Series/all', (req,res) => {
+    Serie.find({}, (err, series) => {
+        if(err) return res.status(400).send(err)
+        res.status(200).send(series)
+    })
+})
+
+app.post('/series/add', (req,res) => {
+    const addserie =  new Serie(req.body)
+    addserie.save((err,doc)=>{
+        if(err) return res.json({succes: false, err})
+        res.status(200).json({
+            succes: true,
+            addserie: doc
+        })
+    })
+})
+
